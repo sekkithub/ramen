88ba00ceb775a19bfe92d84709eae07d6a20b9a8
sekkithub <sekkithub@gmail.com>
2014-10-04 21:09:45 +0000

    add location.js

---
+++ js/location.js
@@ -0,0 +1,7 @@
+var locations = [
+    ['Bone Daddies Ramen Bar', 51.51282, -0.133872],
+    ['Shoryu', 51.510884, -0.134312],
+    ['Ittenbari', 51.510751, -0.137228],
+    ['<h2>Tonkotsu East</h2><p>Address</p>', 51.537058, -0.076212],
+    ['Ramen Seto', 51.536211, -0.139058]
+];
