a702b7a84104b4d783326096cb74714ec9c282ab
sekkithub <sekkithub@gmail.com>
2014-10-22 01:21:34 +0000

    info icon modifying WIP

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $.fn.animateBG = function(x, y, speed) {
 
 
 // Open About modal
-$("#info").click(function() {
+$(".infoIcon").click(function() {
     // List appears delay
     $('.aboutContents ul li').css({
         top : '100px',
