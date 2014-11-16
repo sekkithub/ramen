96489d4595780cc7c59d55a95fccb2eb080c2cbe
sekkithub <sekkithub@gmail.com>
2014-11-16 12:32:23 +0000

    adjust zoom&pan controller position

--- js/mapApi.js
+++ js/mapApi.js
@@ -1,6 +1,7 @@
 var mapOptions = {
   zoom: 14,
   center: new google.maps.LatLng(51.50722, -0.12750),
+  disableDefaultUI: true,
   mapTypeId: google.maps.MapTypeId.ROADMAP,
   mapTypeControl: false,
   zoomControl: true,
@@ -16,9 +17,13 @@ var mapOptions = {
   streetViewControl: false
 }
 
-
 var map = new google.maps.Map(document.getElementById('mapCanvas'),mapOptions);
-google.maps.event.addDomListener(window, mapOptions);
+
+google.maps.event.addDomListener(map, 'tilesloaded', function(){
+    if($('#newPos').length==0){
+        $('div.gmnoprint').last().parent().wrap('<div id="newPos"><div class="ctrl"></div></div>');
+    }
+});
 
 var infowindow = new google.maps.InfoWindow();
 
@@ -26,6 +31,11 @@ var marker, i;
 var markers = new Array();
 var image = 'images/baloon.png';
 var selectedImage = 'images/baloon_active.png';
+
+
+
+
+
 
 for (i = 0; i < shops.length; i++) {  
 
