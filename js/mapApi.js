4a8fe33966a122fa70bbc0dd0aa86bfd465693cb
sekkithub <sekkithub@gmail.com>
2014-11-19 22:30:35 +0000

    add close click event for marker

--- js/mapApi.js
+++ js/mapApi.js
@@ -61,13 +61,22 @@ for (i = 0; i < shops.length; i++) {
     enableEventPropagation: false
   };
 
+  var mmm = marker.getIcon();
   var ib = new InfoBox();
   google.maps.event.addListener(marker, 'click', (function(marker, i) {
     return function() {
-      changeMarkerImage(selectedMarker);
-      function changeMarkerImage() {
+
+
+      if (marker.getIcon() == normalMarker) {
         marker.setIcon(selectedMarker);
-      }
+      } else {
+        marker.setIcon(normalMarker);
+      } 
+
+      google.maps.event.addListener(ib, 'closeclick', function() {
+        marker.setIcon(normalMarker);
+      });
+
 
       ib.setOptions(boxOptions);
 
@@ -86,100 +95,20 @@ for (i = 0; i < shops.length; i++) {
           '</div>' +
         '</div>'
       ;
-
+      
       ib.open(map, marker);
       map.panTo(marker.getPosition({animate: true, duration: 1.0}));
-
     }
-  })(marker, i))  
-}
-
-
-var MarkerImage = ["white.png","black.png","blue.png","yellow.png","green.png","orange.png","pink.png"];
-var myBounds;
-var southEnd, northEnd, westEnd, eastEnd;
-var myMarker = new Array;
-var CurrentMarkerNo = -1;
-
-function initialize() {
-
-   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-   google.maps.event.addDomListenerOnce(map, 'idle', function() {
-      myBounds = map.getBounds();
-      southEnd = myBounds.getSouthWest().lat();
-      northEnd = myBounds.getNorthEast().lat();
-      westEnd = myBounds.getSouthWest().lng();
-      eastEnd = myBounds.getNorthEast().lng();
-      for(var i = 0; i < 50; i++) {
-         setMarker();
-      }
-   });
-
-}
-
-function setMarker() {
-
-   var myImage;
-   var myColorNo;
-   var myLat,myLng;
-   var myLocation;
-   var mNo = myMarker.length;
-
-   //マーカー画像は７色の中からランダムに選択
-   myColorNo = Math.floor(Math.random() * MarkerImage.length);
-   myImage = "icon/"+  MarkerImage[myColorNo];
-
-   //位置もランダムに配置（表示地図中に納まる範囲）
-   myLat = southEnd + (northEnd - southEnd) * Math.random();
-   myLng = eastEnd + (westEnd - eastEnd) * Math.random();
-   myLocation = new google.maps.LatLng(myLat, myLng);
-
-   //マーカー設置（配列に収納）
-   myMarker[mNo] = new google.maps.Marker({
-      map: map,
-      position: myLocation,
-      title: "marker"+mNo,
-      icon: myImage,
-      draggable: false
-   });
-   myMarker[mNo].colorNo = myColorNo; //設置した画像の色Noを登録
-
-   //クリック時処理
-   google.maps.event.addListener(myMarker[mNo], 'click', function() {
-      document.getElementById("marker_lat").value = myLat;
-      document.getElementById("marker_lng").value = myLng;
-      changeMarkerImage(mNo);
-   });
-
-}
-
-function changeMarkerImage(markerNo) {
-
-   //前回の選択マーカーの画像を元に戻す
-   if (CurrentMarkerNo >= 0 && markerNo != CurrentMarkerNo) {
-      var formerImage = "icon/" + MarkerImage[myMarker[CurrentMarkerNo].colorNo];
-      myMarker[CurrentMarkerNo].setIcon(formerImage);
-   }
-   //選択したマーカーの画像を変更
-   myMarker[markerNo].setIcon("icon/l_red.png");
-   CurrentMarkerNo = markerNo; //選択したマーカーの配列番号をグローバル変数にセット
-}
-
-
-
-
-
+  })(marker, i));
 /*
-function changeMarkerImage() {
-   if (CurrentMarkerNo >= 0) {
-     console.log('selected');
-      marker.setIcon(selectedMarker);
-   }
-   console.log('none');
-   marker.setIcon(selectedMarker);
-}
+
+  google.maps.event.addDomListener(marker,'closeclick',(function(marker, i) {
+    return function() {
+      alert('clicked' + shops[i][0])
+    }
+  })(marker, i));
 */
+}
 
 //  Create a new viewpoint bound
 function AutoCenter() {
